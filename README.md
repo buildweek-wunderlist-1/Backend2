bc7e903997bf05a54ab770d99c75e064c413deab
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-08-25 11:01:11 -0600

    API is up and running on port 7777 through the server. i set up the basic file structure and installed all necessary dependencies (i hope).

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Backend2
+
+-- Signup (register)
+POST to `/api/auth/signup
+
+-- Login
+POST to `/api/auth/login
---
+++ auth/auth-router.js
@@ -0,0 +1,10 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const router = require("express").Router();
+
+// const Users = require("../users/users-model.js");
+// const { isValid } = require("../users/users-service.js");
+// const constants = require("../config/constants.js");
+
+module.exports = router;
