fb8022aec3b77fd3a6a5aacc19befb22d91e6722
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-08-25 12:54:14 -0600

    auth router works for register (signup) and login, assigns token properly after hashing password properly. both authentication endpoints work.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,8 +3,80 @@ const jwt = require("jsonwebtoken");
 
 const router = require("express").Router();
 
-// const Users = require("../users/users-model.js");
-// const { isValid } = require("../users/users-service.js");
-// const constants = require("../config/constants.js");
+const Users = require("./auth-model.js");
+const { isValid } = require("./auth-service.js");
+const constants = require("../config/constants.js");
+
+router.post("/signup", (req, res) => {
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    // implement registration
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    // hash password
+    const hash = bcrypt.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    //save user to db
+    Users.add(credentials)
+      .then(user => {
+        res.status(201).json({ data: user });
+      })
+      .catch(error => {
+        res.status(500).json({ errorMessage: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "Please provide username and password. (The password shoud be alphanumeric)",
+    });
+  }
+});
+
+router.post("/login", (req, res) => {
+  // implement login
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        // compare the password and the hash stored in the db
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = signToken(user);
+
+          res.status(200).json({
+            message: "Welcome to the API",
+            token,
+          });
+        } else {
+          res.status(401).json({ message: "Invalid Credentials!" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ errorMessage: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "Please provide username and password. (The password shoud be alphanumeric)",
+    });
+  }
+});
+
+function signToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const secret = constants.jwtSecret;
+
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
--- data/migrations/20200825102812_new_table.js
+++ data/migrations/20200825102812_new_table.js
@@ -1,8 +1,22 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", tbl => {
+      tbl.increments("id");
 
-exports.up = function(knex) {
-  
+      tbl.string("firstName").notNullable();
+      tbl.string("lastName").notNullable();
+      tbl.string("username").notNullable().unique();
+      tbl.string("password").notNullable();
+    })
+    .createTable("tasks", tbl => {
+      tbl.increments("id");
+
+      tbl.string("taskName").notNullable();
+      tbl.date("dueDate").nullable();
+      tbl.boolean("completed").defaultTo(false);
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("tasks").dropTableIfExists("users");
 };
