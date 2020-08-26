9ca280e05b9ae4f9ba654ae1742f2de797491f98
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-08-26 10:55:34 -0600

    Update knexfile.js
    trying to fix deployment on Heroku

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,16 @@
-// // Update with your config settings.
+// Update with your config settings.
+const pgConnection =
+  process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
 module.exports = {
   development: {
     client: "sqlite3",
-    useNullAsDefault: true,
     connection: {
-      filename: "./data/wunderlist.db3",
+      filename: "./data/auth.db3",
     },
+    useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations",
+      tableName: "dbmigrations",
     },
     seeds: {
       directory: "./data/seeds",
@@ -19,20 +22,48 @@ module.exports = {
     },
   },
 
- 
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.db3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
 
-//   production: {
-//     client: "pg",
-//     connection: pgConnection,
-//     pool: {
-//       min: 2,
-//       max: 10,
-//     },
-//     migrations: {
-//       directory: "./data/migrations",
-//     },
-//     seeds: {
-//       directory: "./data/seeds/production",
-//     },
-//   },
-// };
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+};
